cf5be03565c4e0c5d7d2c1b460353a796e3bdfc1
Thomas Peikert <t.s.peikert@gmail.com>
2014-12-07 18:06:03 +0100

    parse csv file and do some basic data handling

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ jawbone_sleep_visualizer
 ========================
 
 Visualize sleep metrics recorded by the jawbone up band
+
+
+Uses the IFTTT recipe that backs up the general sleep metrics recorded by the jawbone up band
+Just add a csv download of the drive spreadsheet and add it to the res folder as sleep.csv
---
+++ main.py
@@ -0,0 +1,57 @@
+__author__ = 'TPei'
+import csv
+import datetime
+
+
+def handle_data():
+    column_names = ['start_date', 'end_date', 'total_sleep_time', 'total_sleep_val', 'light_sleep_time',
+                    'light_sleep_val', 'light_sleep_percent', 'sound_sleep_time', 'sound_sleep_val',
+                    'sound_sleep_percent', 'wake_time', 'wake_val', 'wake_percent', 'graphic']
+
+    # parse csv
+    with open('res/sleep.csv', newline='') as csvfile:
+        sleep_reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        print(column_names)
+
+        for row in sleep_reader:
+            row[0] = str(parse_date(row[0]))
+            row[1] = str(parse_date(row[1]))
+            print(row)
+
+
+def parse_date(date):
+    # used to get month no
+    # '' at beginning of list so that month count starts at 1
+    months = ['', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+    # split by whitespace
+    date = date.split()
+
+    # get hour from hh:mm[AM/PM] substring
+    hour = int(date[4][0:2])
+
+    # convert to 24h format
+    if hour == 12:
+        if date[4][5:7] == 'AM':
+            hour = 0
+    elif date[4][5:7] == 'PM':
+        hour += 12
+
+    # create datetime object
+    date_time = datetime.datetime(int(date[2]), months.index(date[0]), int(date[1][:-1]), hour, int(date[4][3:5]))
+    return date_time
+
+
+# quick and dirty unittest for myself
+def test_parse_date():
+    date = 'December 06, 2014 at 12:17AM'
+    assert parse_date(date) == datetime.datetime(2014, 12, 6, 00, 17)
+
+    date = 'December 06, 2014 at 12:17PM'
+    assert parse_date(date) == datetime.datetime(2014, 12, 6, 12, 17)
+
+    date = 'December 06, 2014 at 01:17PM'
+    assert parse_date(date) == datetime.datetime(2014, 12, 6, 13, 17)
+
+if __name__ == '__main__':
+    handle_data()
